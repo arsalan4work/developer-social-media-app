b6cee633974af5ac7de69b0b6898830f4c8dd979
arsalan4work <m.arsalan4210@gmail.com>
2025-02-25 19:34:03 +0500

    add files

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://e38be44e98e50de0ee1f41050c909e25@o4508727793549317.ingest.us.sentry.io/4508727795843077",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
---
+++ src/sanity/schemaTypes/author.ts
@@ -0,0 +1,44 @@
+import { defineField, defineType } from "sanity";
+import dynamic from "next/dynamic";
+
+const UserIcon = dynamic(() =>
+   import("lucide-react").then((mod) => mod.User)
+ );
+
+export const author = defineType({
+  name: "author",
+  title: "Author",
+  type: "document",
+  icon: UserIcon,
+  fields: [
+    defineField({
+      name: "id",
+      type: "number",
+    }),
+    defineField({
+      name: "name",
+      type: "string",
+    }),
+    defineField({
+      name: "username",
+      type: "string",
+    }),
+    defineField({
+      name: "email",
+      type: "string",
+    }),
+    defineField({
+      name: "image",
+      type: "url",
+    }),
+    defineField({
+      name: "bio",
+      type: "text",
+    }),
+  ],
+  preview: {
+    select: {
+      title: "name",
+    },
+  },
+});
